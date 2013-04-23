a96b7fdfd65af92009a6d7a64878f67bdc0382e9
rgruebel <r.gruebel@bitbeans.de>
2013-04-23 13:02:30 +0200

    test commit

--- rc.lua
+++ rc.lua
@@ -51,7 +51,7 @@ editor_cmd = terminal .. " -e " .. editor
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
-
+  
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
