094268542e489c8da14015793abc8f1e6f5ebc0d
rgruebel <r.gruebel@bitbeans.de>
2013-05-02 09:55:38 +0200

    Bound screen lock (slock) to F12

--- rc.lua
+++ rc.lua
@@ -421,6 +421,7 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
+   awful.key({ modkey }, "F12", function () awful.util.spawn("slock") end),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
     awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
